b65eacf8c15e051662a04ebdb7dc7fa1e3986ed3
Juan Rios <77796276+Juanrios016@users.noreply.github.com>
2025-02-11 18:08:20 -0600

    created class for autonomous part

---
+++ src/main/java/frc/robot/commands/AutonomousInstructions.java
@@ -0,0 +1,29 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.commands.DrivingCommand;
+import frc.robot.subsystems.DrivingSubsytem;
+
+public class AutonomousInstructions extends SequentialCommandGroup { 
+    //extends: main class using to create new command group - using functions from this class
+    
+    /**
+     * Function that holds instruction for robot to execute!
+     */
+    public void AutoRoutine() {
+       
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
