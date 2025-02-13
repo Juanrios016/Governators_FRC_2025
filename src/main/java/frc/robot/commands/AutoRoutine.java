a241dfc9ecca4af5bb2dd0d8f306230300c98935
Team3745 <jhsteam3745@gmail.com>
2025-02-13 16:20:22 -0600

    Cleaning code!

--- src/main/java/frc/robot/commands/AutoRoutine.java
+++ src/main/java/frc/robot/commands/AutoRoutine.java
@@ -1,18 +1,17 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.commands.DrivingCommand;
 import frc.robot.subsystems.DrivingSubsytem;
 
 public class AutoRoutine extends SequentialCommandGroup {
     public AutoRoutine(DrivingSubsytem drive) {
+
         addCommands(
-            new DrivingCommand(()-> 1, ()-> 1, drive, Timer.getFPGATimestamp(), 10.0), // Move forward 3 meters
+            new DrivingCommand(()-> 1, ()-> 1, drive, 5.0, false), // Move forward 3 meters
             new WaitCommand(5), // Pause for stability
-            new DrivingCommand(()-> 0.5, ()-> 0.5, drive, Timer.getFPGATimestamp(), 5.0)
+            new DrivingCommand(()-> 0.5, ()-> 0.5, drive, 5.0, false)
         );
+
     }
 }
